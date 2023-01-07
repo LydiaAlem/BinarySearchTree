c9de1c46edfbab7ec1c060c8231ce6b3500bc0e5
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2023-01-06 18:36:01 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 /**
  * This class extends {@BinaryTree.java} to ask the user for inputs from the BST
- * By: Lydia Alem
  */
 
 import java.util.*;
@@ -8,21 +7,64 @@ import java.util.*;
 public class Main<T extends Comparable<T>>{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("How many nodes do you want in the BST: ");
-        int numNodes = scan.nextInt();
-
         BinaryTree<Integer> tree = new BinaryTree<>();
-        for(int i = 0; i < numNodes; i++){
-            System.out.print("Enter the value of node " + (i+1) + ": ");
-            int value = scan.nextInt();
-            tree.insert(value);
+
+        while(true){
+            try{
+                System.out.println("\nMenu:");
+                System.out.println("1. Insert a node");
+                System.out.println("2. Remove a node");
+                System.out.println("3. Search for a node");
+                System.out.println("4. Traverse the tree");
+                System.out.println("5. Quit");
+                System.out.print("> ");
+
+                int choice = scan.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        // Insert a node
+                        System.out.print("Enter the value of the node to insert: ");
+                        int value = scan.nextInt();
+                        tree.insert(value);
+                        break;
+                    case 2:
+                        // Remove a node
+                        System.out.print("Enter the value of the node to remove: ");
+                        value = scan.nextInt();
+                        tree.remove(tree.getRoot(), value);
+                        break;
+                    case 3:
+                        // Search for a node
+                        System.out.print("Enter the value of the node to search for: ");
+                        value = scan.nextInt();
+                        if (tree.contains(value)) {
+                            System.out.println("Node found");
+                        } else {
+                            System.out.println("Node not found");
+                        }
+                        break;
+                    case 4:
+                        // Traverse the tree
+                        System.out.println("In-Order Transversal:");
+                        tree.inOrderTraversal();
+                        System.out.println("\n\nPre-Order Transversal:");
+                        tree.preOrderTraversal();
+                        System.out.println("\n\nPost-Order Transversal:");
+                        tree.postOrderTraversal();
+                        break;
+                    case 5:
+                        // Quit
+                        scan.close();
+                        return;
+                    default:
+                        // Invalid choice
+                        System.out.println("Invalid choice. Please try again.");
+            }
+        } catch (InputMismatchException e){
+            System.out.println("Invalid input. Please try again.");
+            scan.nextLine(); // Discard invalid input
         }
-        System.out.println("In-Order Transversal:");
-        tree.inOrderTraversal();
-        System.out.println("\n\nPre-Order Transversal:");
-        tree.preOrderTraversal();
-        System.out.println("\n\nPost-Order Transversal:");
-        tree.postOrderTraversal();
-        scan.close();
-    }
+        }
+    }   
 }
